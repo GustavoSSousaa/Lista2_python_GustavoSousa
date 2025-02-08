1acca194f5922ffe6bebaf8ee794b00b92d0f5a2
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-02-07 21:18:33 -0300

    Novos

--- Exe08_GustavoSousa.py
+++ Exe08_GustavoSousa.py
@@ -1,4 +1,4 @@
 #Escreva umm programa que peça ao usuario para inserir 2 numeros inteiro, some-os e exiba a mensagem : 0 tal é (soma dos valor)
-n1=int(input("Escreva o 1° numero"))
-n2=int(input("Escreva o 2° numero"))
+n1=int(input("Escreva o 1° numero : "))
+n2=int(input("Escreva o 2° numero : "))
 print ("O total é :",n1 + n2)
--- Exe10_GustavoSousa.py
+++ Exe10_GustavoSousa.py
@@ -1,7 +1,6 @@
 #Escreva um programa que pergunte quantos pedaço o bolo tem, em seguida pergunte ao usuario quantos pedaço ele comeu , calcule quantos pedaços ainda tem e exiba o resultado com uma mensagem de livre escolha
 n1=int(input ("Digite quantos bolo voce comeu : "))
-n2=int(input ("Digite quantos tinha o bolo : "))
-conta = n2 - n1
-print ("Voce comeu ",n1)
-print ("O bolo tinha  ",n2,"pedaços")
-print ("Ainda faltam ",conta)
+n2=int(input ("Digite quantos pedaços tinha o bolo : "))
+print ("Voce comeu",n1)
+print ("O bolo tinha",n2,"pedaços do bolo")
+print ("Ainda faltam ",n2 - n1)
